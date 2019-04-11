9dcb6a71e038b0fc684927dc99ce24840f5dc981
Michael Armes <michael@un-loop.org>
2019-04-11 15:12:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const convert = require('koa-convert');
+const Resource = require('koa-resource-router');
+const checkPermission = require("unloop-check-permission").roles;
+
+const wrapExport = (exp, permissions, entryPoint) =>
+    function*(next) {
+        if (permissions) {
+            if (!checkPermission(this, permissions.default) || !checkPermission(this, permissions[entryPoint])) {
+                this.throw(403, "Not Authorized");
+            }
+        }
+
+        yield convert.back(exp[entryPoint]);
+    }
+
+module.exports = (basePath) => (...middleware) => (entity) => {
+
+    const resourceModule = require(`${basePath}/resources/${entity}.js`);
+    const importedResorce = resourceModule(require(`${basePath}/entity/${entity}.js`));
+
+    const builtResource = {};
+    for(let entryPoint in importedResorce) {
+        builtResource[entryPoint] = wrapExport(importedResorce, resourceModule.permissions, entryPoint);
+    }
+
+    return new Resource(entity, ...middleware, builtResource);
+}
